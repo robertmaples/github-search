17b067ec18c65f32c289d4f5d0048a7bfa924244
Robert Maples <robert.maples@armoredthings.com>
2022-01-26 08:36:59 -0500

    search UI completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+// @ts-ignore
+import GithubLogo from "./resources/github-logo.png";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Page>
+      <Container>
+        <Search>
+          <div>
+            <SearchDescriptor>
+              <Logo src={GithubLogo} />
+              <div>
+                <Title>GitHub Searcher</Title>
+                <Subtitle>Search users or repositories below</Subtitle>
+              </div>
+            </SearchDescriptor>
+            <div>
+              <Input type={"search"} placeholder="Start typing to search .." />
+              <Dropdown>
+                <option>Users</option>
+                <option>Repositories</option>
+              </Dropdown>
+            </div>
+          </div>
+        </Search>
+        <Results>Results</Results>
+      </Container>
+    </Page>
   );
-}
+};
 
 export default App;
+
+const Page = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #313639;
+  font-size: 9pt;
+`;
+const Container = styled.div`
+  width: 75%;
+  height: 75%;
+`;
+const Search = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Title = styled.div`
+  font-weight: bold;
+  font-size: 11pt;
+`;
+const Subtitle = styled.div`
+  color: #a9a9a9;
+`;
+const SearchDescriptor = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+const Logo = styled.img`
+  width: 50px;
+  height: 50px;
+`;
+const Dropdown = styled.select`
+  height: 35px;
+  margin-left: 10px;
+`;
+const Input = styled.input`
+  height: 35px;
+  width: 250px;
+`;
+const Results = styled.div``;
